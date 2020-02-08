a0117adda9ea188092b93d59906722be095a3193
MinexAutomation <minex.automation@gmail.com>
2020-02-07 16:22:57 -0800

    Added IServiceCollection extensions.

--- source/R5T.Solitude.Soltana/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Solitude.Soltana
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Solitude.Soltana/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using InMemorySolutionFileOperator = R5T.Soltana.IVisualStudioSolutionFileOperator;
+
+
+namespace R5T.Solitude.Soltana
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileOperator"/> implementation of <see cref="IVisualStudioSolutionFileOperator"/> as an <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddSoltanaVisualStudioSolutionFileOperator(this IServiceCollection services,
+            ServiceAction<InMemorySolutionFileOperator> addInMemorySolutionFileOperator,
+            ServiceAction<IVisualStudioSolutionFileOperator> addSolutionFileSerializer)
+        {
+            services
+                .AddSingleton<IVisualStudioSolutionFileOperator, VisualStudioSolutionFileOperator>()
+                .RunServiceAction(addInMemorySolutionFileOperator)
+                .RunServiceAction(addSolutionFileSerializer)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileOperator"/> implementation of <see cref="IVisualStudioSolutionFileOperator"/> as an <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileOperator> AddSoltanaVisualStudioSolutionFileOperatorAction(this IServiceCollection services,
+            ServiceAction<InMemorySolutionFileOperator> addInMemorySolutionFileOperator,
+            ServiceAction<IVisualStudioSolutionFileOperator> addSolutionFileSerializer)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileOperator>(() => services.AddSoltanaVisualStudioSolutionFileOperator(
+                addInMemorySolutionFileOperator,
+                addSolutionFileSerializer));
+            return serviceAction;
+        }
+    }
+}

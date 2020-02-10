d9ce25437c46f28145ef0df56747ca23ac8d01ad
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:28:05 -0800

    Implemented all solution folder path functionality.

--- source/R5T.Solitude.Soltana/Code/Services/Implementations/VisualStudioSolutionFileOperator.cs
+++ source/R5T.Solitude.Soltana/Code/Services/Implementations/VisualStudioSolutionFileOperator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using R5T.Cambridge.Types;
 using R5T.Soltana.Extensions;
@@ -44,11 +45,11 @@ namespace R5T.Solitude.Soltana
             return output;
         }
 
-        public void AddProjectFile(string solutionFilePath, string projectFilePath, Guid projectTypeGuid)
+        public void AddProjectFile(string solutionFilePath, string projectFilePath, Guid projectTypeGuid, Guid projectGuid)
         {
             this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
             {
-                this.InMemorySolutionFileOperator.AddProjectFile(solutionFile, solutionFilePath, projectFilePath, projectTypeGuid);
+                this.InMemorySolutionFileOperator.AddProjectFile(solutionFile, solutionFilePath, projectFilePath, projectTypeGuid, projectGuid);
             });
         }
 
@@ -63,13 +64,13 @@ namespace R5T.Solitude.Soltana
         {
             var solutionFile = this.InMemorySolutionFileOperator.CreateNewSolutionFile();
 
-            var hasProjectFile = this.InMemorySolutionFileOperator.HasProjectFile(solutionFile, solutionFilePath, projectFilePath);
+            var hasProjectFile = this.InMemorySolutionFileOperator.HasProjectFile(solutionFile, solutionFilePath, projectFilePath, out _);
             return hasProjectFile;
         }
 
         public IEnumerable<string> ListProjectReferenceFilePaths(string solutionFilePath)
         {
-            var solutionFile = this.InMemorySolutionFileOperator.CreateNewSolutionFile();
+            var solutionFile = this.VisualStudioSolutionFileSerializer.Deserialize(solutionFilePath);
 
             var projectReferenceFilePaths = this.InMemorySolutionFileOperator.ListProjectFilePaths(solutionFile, solutionFilePath);
             return projectReferenceFilePaths;
@@ -84,6 +85,104 @@ namespace R5T.Solitude.Soltana
             });
 
             return success;
+        }
+
+        public void AddSolutionFolder(string solutionFilePath, string solutionFolderPath)
+        {
+            this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                this.InMemorySolutionFileOperator.AddSolutionFolder(solutionFile, solutionFolderPath);
+            });
+        }
+
+        public bool HasSolutionFolder(string solutionFilePath, string solutionFolderPath)
+        {
+            var hasSolutionFolder = this.ModfifySolutionFile(solutionFolderPath, (solutionFile) =>
+            {
+                var output = this.InMemorySolutionFileOperator.HasSolutionFolder(solutionFile, solutionFolderPath, out _);
+                return output;
+            });
+
+            return hasSolutionFolder;
+        }
+
+        public bool RemoveSolutionFolderAndContents(string solutionFilePath, string solutionFolderPath)
+        {
+            var success = this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                var output = this.InMemorySolutionFileOperator.RemoveSolutionFolderAndContents(solutionFile, solutionFolderPath);
+                return output;
+            });
+
+            return success;
+        }
+
+        public void MoveProjectFileIntoSolutionFolder(string solutionFilePath, string projectFilePath, string solutionFolderPath)
+        {
+            this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                this.InMemorySolutionFileOperator.MoveProjectFileIntoSolutionFolder(solutionFile, solutionFilePath, projectFilePath, solutionFolderPath);
+            });
+        }
+
+        public void MoveProjectFileOutOfSolutionFolder(string solutionFilePath, string projectFilePath, string solutionFolderPath)
+        {
+            this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                this.InMemorySolutionFileOperator.MoveProjectFileOutOfSolutionFolder(solutionFile, solutionFilePath, projectFilePath, solutionFolderPath);
+            });
+        }
+
+        public IEnumerable<string> ListSolutionFolderProjectFilePaths(string solutionFilePath, string solutionFolderPath)
+        {
+            var projectFiles = this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                var projectFileReferences = this.InMemorySolutionFileOperator.ListSolutionFolderProjectFiles(solutionFile, solutionFilePath, solutionFolderPath);
+
+                var output = projectFileReferences.Select(x => x.ProjectFilePathValue).ToArray();
+                return output;
+            });
+
+            return projectFiles;
+        }
+
+        public IEnumerable<string> ListSolutionFolderSolutionFolderNames(string solutionFilePath, string solutionFolderPath)
+        {
+            var solutionFolderNames = this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                var solutionFolderProjects = this.InMemorySolutionFileOperator.ListSolutionFolderSolutionFolders(solutionFile, solutionFolderPath);
+
+                var output = solutionFolderProjects.Select(x => x.ProjectName);
+                return output;
+            });
+
+            return solutionFolderNames;
+        }
+
+        public IEnumerable<string> ListRootSolutionFolderNames(string solutionFilePath)
+        {
+            var rootSolutionFolderNames = this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                var rootSolutionFolders = this.InMemorySolutionFileOperator.ListRootSolutionFolders(solutionFile);
+
+                var output = rootSolutionFolders.Select(x => x.ProjectFileRelativePathValue);
+                return output;
+            });
+
+            return rootSolutionFolderNames;
+        }
+
+        public IEnumerable<string> ListRootProjectFilePaths(string solutionFilePath)
+        {
+            var rootProjectFilePaths = this.ModfifySolutionFile(solutionFilePath, (solutionFile) =>
+            {
+                var rootProjectReferences = this.InMemorySolutionFileOperator.ListRootProjectFiles(solutionFile, solutionFilePath);
+
+                var output = rootProjectReferences.Select(x => x.ProjectFilePathValue).ToArray();
+                return output;
+            });
+
+            return rootProjectFilePaths;
         }
     }
 }
